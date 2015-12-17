22fb4e2784cae87c091430c78db594b4aacd174b
admin <mattes.standfuss@sitec-systems.de>
2015-12-17 11:48:13 +0100

    - License information added (Ticket #866)

--- src/main/java/de/sitec/jsocketcan/jna/InterfaceRequestStruct.java
+++ src/main/java/de/sitec/jsocketcan/jna/InterfaceRequestStruct.java
@@ -1,11 +1,26 @@
+/**
+ * jSocketCan is an Framework for Access to Linux SocketCAN over JNA. 
+ * Implements jCanInterface.
+ * 
+ * Copyright (C) 2015 sitec systems GmbH <http://www.sitec-systems.de>
+ * 
+ * This file is part of jSocketCan.
+ * 
+ * jSocketCan is free software: you can redistribute it and/or modify it 
+ * under the terms of the GNU Lesser General Public License as published by the 
+ * Free Software Foundation, either version 3 of the License, or (at your option) 
+ * any later version.
+ * 
+ * jSocketCan is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with jSocketCan. If not, see <http://www.gnu.org/licenses/>.
+ */
 /*
- * Project: 15_0013_CF4J
- * $Header: $
- * Author: Robert Lehmann
  * Author: Mattes Standfuss
- * Last Change:
- *    by:   $Author: $
- *    date: $Date:   $
  * Copyright (c): sitec systems GmbH, 2015
  */
 package de.sitec.jsocketcan.jna;
